f4f2a0bd47ce85036a420142da76726a160710d1
Tommager1107 <147331759+Tommager1107@users.noreply.github.com>
2024-06-01 20:26:47 +0200

    Update script.js

--- dowload/script.js
+++ dowload/script.js
@@ -1,10 +1,10 @@
 document.getElementById("download-btn").addEventListener("click", function() {
     var windowsVersion = document.getElementById("windows-version").value;
     if (windowsVersion === "windows10") {
-        // Replace 'windows10_download_link' with the actual download link for Windows 10
+        
         window.location.href = "https://github.com/Tommager1107/windows-optimalization/releases/download/1.0.0/Windows10-OptimalizationSoftware.exe";
     } else if (windowsVersion === "windows11") {
-        // Replace 'windows11_download_link' with the actual download link for Windows 11
+        
         window.location.href = "win11.html";
     }
 });
